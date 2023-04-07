573eb29abed41e08c61e7d9f37e1b464f3abc25e
unknown <asifs.cse@gmail.com>
2023-04-07 09:55:42 +0530

    access variable

--- React/CreateApp/my-app/src/index.js
+++ React/CreateApp/my-app/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const todoTitle = "Call Family";
+const doToDescription ="Welcome to my blog, This is Asif Shahriar";
+const date = new Date();
+
 root.render(
-  <h1>Welcome to React</h1>
+  <div>
+    <h1>Welcome to React</h1>
+    <h3>{todoTitle}</h3>
+    <h3>{date}</h3>
+    <p>{doToDescription}</p>
+    <p>{new Date().getFullYear}</p>
+  </div>
+  
 );
 
