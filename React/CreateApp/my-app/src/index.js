c74a56589b16bfc1b4aa794008ebf51b3e8808ef
unknown <asifs.cse@gmail.com>
2023-04-07 10:08:29 +0530

    external css in pulic folder

--- React/CreateApp/my-app/src/index.js
+++ React/CreateApp/my-app/src/index.js
@@ -8,17 +8,17 @@ const doToDescription ="Welcome to my blog, This is Asif Shahriar";
 const date = new Date();
 
 //css style variable
-const headingStyle = {
-  backgroundColor: "purple",
-  color: "white",
-  textAlign: "center",
-  padding: "15px"
-}
+// const headingStyle = {
+//   backgroundColor: "purple",
+//   color: "white",
+//   textAlign: "center",
+//   padding: "15px"
+// }
 
 root.render(
   <div>
     <h1 style={headingStyle}>Welcome to React</h1>
-    <h3>{todoTitle}</h3>
+    <h3 className='headingStyle'>{todoTitle}</h3>
     <h3>{date}</h3>
     <p>{doToDescription}</p>
     <p>{new Date().getFullYear}</p>
