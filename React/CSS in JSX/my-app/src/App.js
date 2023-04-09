cfe9ec978065239ca1e264b95f0ec39a4e0e74b7
unknown <asifs.cse@gmail.com>
2023-04-10 00:07:26 +0530

    receave json data and show

--- React/CSS in JSX/my-app/src/App.js
+++ React/CSS in JSX/my-app/src/App.js
@@ -1,9 +1,13 @@
 import React  from "react";
 import Card from './components/Card'
+import data from './data.json'
+
+
 
 function App(){
   return <div>
           <h1 className='headingStyle'>Todo App</h1>
+          <Card titleText={data[0].title} descText = {data[0].desc}/>
           <Card titleText="ToDo App" descText="Welcome to our ToDo App"/>
           <Card titleText="ToDo App" descText="Welcome to our ToDo App"/>
           <Card titleText="ToDo App" descText="Welcome to our ToDo App"/>
