4a1f272f5c7288dfa004f69c42b77b724870e8be
unknown <asifs.cse@gmail.com>
2023-04-09 23:45:47 +0530

    receive value using props

---
+++ React/CSS in JSX/my-app/src/components/Card.js
@@ -0,0 +1,15 @@
+const date = new Date();
+const dateName = date.getDate();
+const monthName = date.getMonth();
+const yearName = date.getFullYear();
+
+function Card(props){
+  const {titleText, descText} = props;
+  return <div className='card'>
+          <h3 className='cardTitle'>{titleText}</h3>
+          <p className='cardDesc'>{descText}</p>
+          <p className='cardFooter'>{dateName+"/"+monthName+"/"+yearName}</p>
+          </div>
+}
+
+export default Card;
--- React/CSS in JSX/my-app/src/index.js
+++ React/CSS in JSX/my-app/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Card from './Card'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <div>
-    <h1 className='headingStyle'>Todo App</h1>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    
+    <App />
   </div>
 );
+
+ReactDOM.render(<App />, document.getElementById('root'));
